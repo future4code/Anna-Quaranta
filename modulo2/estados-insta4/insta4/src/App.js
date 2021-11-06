2edcb0f09a70def74e190c18b538993047dfe2e4
Anna Isabella Gomes Quaranta <bellaquaranta@gmail.com>
2021-11-05 21:34:47 -0300

    Exercicio feito

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -8,26 +8,89 @@ const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `
-
+const InputEstilizada = styled.input`
+  margin: 5px 0;
+`
 class App extends React.Component {
+  state = {
+
+    posts : [
+      {
+        nome : "paulinha",
+        fotoDoUsuario : "https://picsum.photos/50/50",
+        fotoDoPost : "https://picsum.photos/200/150?a=1"
+      },
+      {
+        nome :"isabella",
+        fotoDoUsuario :"https://picsum.photos/50/50?a=4",
+        fotoDoPost :"https://picsum.photos/200/150?a=1"
+      },
+      {
+        nome : "beatriz",
+        fotoDoUsuario : "https://picsum.photos/50/50?a=2",
+        fotoPost : "https://picsum.photos/200/150?a=1"
+      },
+    ],
+
+    inputNome: '',
+    inputFotoUsuario: '',
+    inputFotoPost: ''
+  
+  
+  }
+
+
+  onChangeSalvarNome = (event) =>{
+    this.setState({inputNome : event.target.value})
+    console.log(event.target.value)
+
+  }
+  onChangeSalvarFotoDoUsuario = (event) =>{
+    this.setState({inputFotoUsuario : event.target.value})
+    console.log(event.target.value)
+
+  }
+  onChangeSalvarFotoPost= (event) =>{
+    this.setState({inputFotoPost : event.target.value})
+    console.log(event.target.value)
+
+  }
+
+  onClickEnviarBotao = () => {
+    const novoObjeto = {nome: this.state.inputNome,
+      fotoDoUsuario : this.state.inputFotoUsuario,
+      fotoDoPost : this.state.inputFotoPost
+    }
+
+    const novoPost = [...this.state.posts, novoObjeto]
+
+    this.setState({posts: novoPost})
+  }
   render() {
+
+    const listaDePosts = this.state.posts.map((post,index) =>{
+      return (
+        <Post key = {index}
+          nomeUsuario = {post.nome}
+          fotoUsuario = {post.fotoDoUsuario}
+          fotoPost = {post.fotoPost}
+        />
+      )
+
+    })
+
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'isabella'}
-          fotoUsuario={'https://picsum.photos/50/51'}
-          fotoPost={'https://picsum.photos/200/151'}
-        />
-        <Post
-          nomeUsuario={'beatriz'}
-          fotoUsuario={'https://picsum.photos/50/52'}
-          fotoPost={'https://picsum.photos/200/152'}
-        />
+        {listaDePosts}
+        <InputEstilizada placeholder="Nome" value={this.state.inputNome} onChange={this.onChangeSalvarNome}/>
+
+        <InputEstilizada placeholder="Foto do Usuario" value={this.state.inputFotoUsuario} onChange={this.onChangeSalvarFotoDoUsuario}/>
+
+        <InputEstilizada placeholder="Foto do Post" value={this.state.inputFotoPost} onChange = {this.onChangeSalvarFotoPost}/>
+
+        <button onClick = {this.onClickEnviarBotao}>Enviar</button>
+
+
       </MainContainer>
     );
   }
