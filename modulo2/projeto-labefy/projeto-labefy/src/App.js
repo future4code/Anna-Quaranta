e25f3e1e9863e68648bb78e5418ece545e5add74
Anna Isabella Gomes Quaranta <bellaquaranta@gmail.com>
2021-11-18 14:48:13 -0300

    Funcionalidade renderizar na tela a lista de playlists e apagar a playlist que quiser

--- modulo2/projeto-labefy/projeto-labefy/src/App.js
+++ modulo2/projeto-labefy/projeto-labefy/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CriarPlaylist from "./Components/CriarPlaylist";
+import ListaDePlaylists from "./Components/ListaDePlaylists";
 
 export default class App extends React.Component {
   render(){
     return(
       <div>
-        <CriarPlaylist/>
+        {/* <CriarPlaylist/> */}
+        <ListaDePlaylists/>
       </div>
     )
   }
